f3ef19b35dafcdfc23a1c007bd4ba68644bd6198
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2026-05-21 19:16:33 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length())
+            return false;
+
+        StringBuilder st = new StringBuilder(t);
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            int index = st.indexOf(String.valueOf(ch));
+
+            if (index == -1)
+                return false;
+
+            st.deleteCharAt(index);
+        }
+
+        return true;
+    }
+}
