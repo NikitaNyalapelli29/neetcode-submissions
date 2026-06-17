84e4e1f55d99446c7a597c510983733a0356c447
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2026-06-17 19:46:40 +0530

    Add: boats-to-save-people - submission-3

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+       int count =0;
+       Arrays.sort(people);
+       int l=0, r = people.length-1;
+       while(l<=r){
+        if(people[l]+people[r]<=limit){
+            //count++;
+            l++;
+        }
+        r--;
+        count++;
+
+       }
+        return count;
+    }
+}
