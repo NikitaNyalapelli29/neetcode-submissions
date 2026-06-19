5508fe5f38b2008edd3a0d27d862a00ff2ef7c11
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2026-06-19 22:33:08 +0530

    Add: majority-element-ii - submission-4

---
+++ Data Structures & Algorithms/majority-element-ii/submission-4.java
@@ -0,0 +1,41 @@
+
+
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        if (nums == null || nums.length == 0) return list;
+
+        int count1 = 0, count2 = 0;
+        int ele1 = 0, ele2 = 0; 
+
+        for (int num : nums) {
+            if (num == ele1) {
+                count1++;
+            } else if (num == ele2) {
+                count2++;
+            } else if (count1 == 0) {
+                ele1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                ele2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        count1 = count2 = 0;
+        for (int num : nums) {
+            if (num == ele1) {
+                count1++;
+            } else if (num == ele2) {
+                count2++; 
+            }
+        }
+        if (count1 > nums.length / 3) list.add(ele1);
+        if (count2 > nums.length / 3) list.add(ele2);
+
+        return list;
+    }
+}
