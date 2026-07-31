360f9ea44c1fc2a579d783f08c3a7513473a4626
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2026-07-31 10:05:43 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int maxProfit = 0;
+        int n = prices.length;
+        int minPrice = prices[0];
+        for(int i=1; i<n; i++){
+           if(minPrice > prices[i]) 
+                minPrice = prices[i];
+            if(maxProfit < prices[i]-minPrice)
+                maxProfit = prices[i]-minPrice;
+        }
+        return maxProfit;
+    }
+}
